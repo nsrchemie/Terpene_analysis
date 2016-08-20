5170953685e2184295b4a05279c956b92c57b1dc
nsrchemie <nsrchemie@gmail.com>
2016-08-19 21:41:41 -0400

    initial setup, grepped file added, parser spits out text as vector

---
+++ parser.clj
@@ -0,0 +1,8 @@
+(require '[clojure.string :as str])
+(require '[clojure.java.io :as io])
+
+(defn secread [x]
+(with-open [rdr (io/reader "./scraped")]
+(doseq [line (line-seq rdr)]
+(println (str/split line #" ")))
+))
