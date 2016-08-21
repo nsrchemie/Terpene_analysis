243bc4d2bb9745dfaaafe1b2a872a447b11b5114
nsrchemie <nsrchemie@gmail.com>
2016-08-20 23:33:59 -0400

    Finally getting complete molecule names without the word /belongs/ being attached, still need to work on compound class

--- parser.clj
+++ parser.clj
@@ -1,10 +1,27 @@
 (require '[clojure.string :as str])
 (require '[clojure.java.io :as io])
 
+
+(defn is-small? [val]
+	(if (<= (count val) 4)
+		true
+		false))
+
 (defn secread [x]
 (with-open [rdr (io/reader "./scraped")]
 (doseq [line (line-seq rdr)]
-(println (get (str/split (clojure.string/replace line 
-	#"./hmdb_metabolites.xml:" "") #" ") 0), 
-(get (str/split line #"family of ") 1)))
-))
+
+(println 
+	(get (str/split (clojure.string/replace line 
+	#"./hmdb_metabolites.xml:" "") #" ") 0),
+	(clojure.string/replace (get (str/split (clojure.string/replace line 
+	 #"./hmdb_metabolites.xml:" "") #" ") 1) #"belongs" ""))
+
+; (if (is-small? (get (str/split (clojure.string/replace line 
+; 	 #"./hmdb_metabolites.xml:" "") #" ") 1)) 
+; println (get (str/split (clojure.string/replace line 
+; 	 #"./hmdb_metabolites.xml:" "") #" ") 1)))
+
+
+; (get (str/split line #"family of ") 1)
+)))
